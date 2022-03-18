2aea50c615a1a0c6c304cfa852bfae47da7f0956
Vasileios Papavasileiou <vpapav96@gmail.com>
2022-03-18 18:08:37 +0200

    Add exclude fields functionality

--- parser.go
+++ parser.go
@@ -40,6 +40,34 @@ func selectFieldsRecursively(attributesMap map[string]interface{}, object interf
 	return nil
 }
 
+func excludeFieldsRecursively(attributesMap map[string]interface{}, object interface{}) {
+	//TODO: create new object instead of modifying the existing one
+	if len(attributesMap) == 0 {
+		return
+	}
+	switch o := object.(type) {
+	case map[string]interface{}:
+		for field := range attributesMap {
+			newAttributesMap := attributesMap[field].(map[string]interface{})
+			value, ok := o[field]
+			if !ok {
+				continue
+			}
+			if len(newAttributesMap) == 0 {
+				delete(o, field)
+				continue
+			}
+			if value != nil {
+				excludeFieldsRecursively(newAttributesMap, value)
+			}
+		}
+	case []interface{}:
+		for _, item := range o {
+			excludeFieldsRecursively(attributesMap, item)
+		}
+	}
+}
+
 func createAttributesMap(attributesList []string) map[string]interface{} {
 	attributesMap := make(map[string]interface{})
 	for _, attributes := range attributesList {
@@ -60,6 +88,14 @@ func SelectFields(attributesList []string, data interface{}) interface{} {
 	if len(attributesMap) > 0 && data != nil {
 		modifiedData := selectFieldsRecursively(attributesMap, data)
 		return modifiedData
+	}
+	return data
+}
+
+func ExcludeFields(attributesList []string, data interface{}) interface{} {
+	attributesMap := createAttributesMap(attributesList)
+	if len(attributesMap) > 0 && data != nil {
+		excludeFieldsRecursively(attributesMap, data)
 	}
 	return data
 }
